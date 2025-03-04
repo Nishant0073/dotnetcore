7c7405eca0c58b17e38eae390dce2c9ba35843ca
Nishant Shingate <nishantks121@gmail.com>
2025-03-04 23:02:16 +0530

    multiple methods

--- ControllersExample/Controllers/HomeController.cs
+++ ControllersExample/Controllers/HomeController.cs
@@ -3,10 +3,20 @@ using Microsoft.AspNetCore.Mvc;
 namespace ControllersExample.Controllers
 {
     public class HomeController{
-        [Route("sayhello")]
-        public string method1(){
-            return "Hello from method1";
+        [Route("/")]
+        public string Index(){
+            return "Hello from index";
+        }
+
+        [Route("/about")]
+        public string About(){
+            return "Hello from about";
+        }
+        [Route("/contact-us")]
+        public string Contact(){
+            return "Hello from contact";
         }
     }
      
 }
+
