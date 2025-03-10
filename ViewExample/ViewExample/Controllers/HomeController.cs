938bb0aa4249aec3af3a2c1ef3fc3e5f991942f9
Nishant Shingate <nishantks121@gmail.com>
2025-03-10 22:00:45 +0530

    ViewData part 1

--- ViewExample/ViewExample/Controllers/HomeController.cs
+++ ViewExample/ViewExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewExample.Models;
 
 namespace ViewExample.Controllers
 {
@@ -7,6 +8,28 @@ namespace ViewExample.Controllers
         [Route("home")]
         public IActionResult Index()
         {
+            ViewData["title"] = "View Example Rozer views";
+            ViewData["people"] = new List<Person>()
+            {
+            new Person()
+            {
+                PersonName = "Ram",
+                DOB = Convert.ToDateTime("2002/12/12"),
+                Gender =  "Male"
+            },
+            new Person()
+            {
+                PersonName = "Shyam",
+                //DOB = Convert.ToDateTime("2000/02/11"),
+                Gender =  "Male"
+            },
+            new Person()
+            {
+                PersonName = "Sita",
+                DOB = Convert.ToDateTime("2001/05/09"),
+                Gender =  "Female"
+            }
+            };
             return View();
         }
     }
