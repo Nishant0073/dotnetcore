4048df918aad1bfe795153cd46c2da9919d881e9
Nishant Shingate <nishantks121@gmail.com>
2025-03-12 21:52:47 +0530

    Added Layout

---
+++ LayoutExample/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutExample.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("/products")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/search-products")]
+        public IActionResult Search()
+        {
+            return View();
+        }
+        [Route("/order-products")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}
