50cb78ae89916f514b01ef035d4ecbcbcca44ae3
Nishant Shingate <nishantks121@gmail.com>
2025-03-18 23:49:17 +0530

    feature: ViewComponentResult

---
+++ ViewComponentExample/wwwroot/friends.js
@@ -0,0 +1,5 @@
+﻿document.querySelector("#friendlist_button").addEventListener("click",async function () {
+    var response = await fetch("friends-list")
+    var body = await response.text();
+    document.querySelector(".friends_list").innerHTML = body;
+});
