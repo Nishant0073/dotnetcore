ceae37c348eacad6dfdd95ab5db187660bf2aff6
Nishant Shingate <nishantks121@gmail.com>
2025-03-18 23:28:15 +0530

    feature: Supply Parametrs to vc.

--- ViewComponentExample/ViewComponents/GridViewComponent.cs
+++ ViewComponentExample/ViewComponents/GridViewComponent.cs
@@ -5,31 +5,9 @@ namespace ViewComponentExample.ViewComponents
 {
     public class GridViewComponent : ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync(PersonGrid grid)
         {
-            PersonGrid grid = new PersonGrid()
-            {
-                Title = "Employees",
-                Persons = new List<Person>()
-                {
-                    new Person
-                    {
-                        PersonName = "Shyam",
-                        JobTitle = "Manager"
-                    },
-                    new Person
-                    {
-                        PersonName = "Ram",
-                        JobTitle = "CEO"
-                    },
-                    new Person
-                    {
-                        PersonName = "Krishna",
-                        JobTitle = "Ass. Manager"
-                    },
-                }
-
-            };
+           
             return View("Sample",grid);
         }
     }
