cfae44ca2e9bbb2771a6b671bc45da0f332c5f47
Nishant Shingate <nishantks121@gmail.com>
2025-03-19 23:55:33 +0530

    Created Service

---
+++ CitiesService/CitiesService.cs
@@ -0,0 +1,23 @@
+﻿namespace CitiesService
+{
+    public class MyCitiesService
+    {
+        private List<string> cities;
+        public MyCitiesService()
+        {
+            cities = new List<string>()
+            {
+                "New York",
+                "Los Angeles",
+                "Chicago",
+                "Houston",
+            };
+        }
+
+        public List<string> GetCities()
+        {
+            return cities;
+        }
+
+    }
+}
---
+++ ServiceExample/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CitiesService;
+
+namespace ServiceExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly MyCitiesService citiesService;
+        public HomeController()
+        {
+            citiesService = new MyCitiesService();
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            var cities = citiesService.GetCities();
+            return View(cities);
+        }
+    }
+}
