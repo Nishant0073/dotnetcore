16d91f26e327f8c06033446c64bbfc60208dbed3
Nishant Shingate <nishantks121@gmail.com>
2025-04-03 22:16:54 +0530

    Added Basic UI and Service

---
+++ StockApi/Service/StockService.cs
@@ -0,0 +1,27 @@
+﻿namespace StockApi.Service
+{
+    public class StockService
+    {
+        private IHttpClientFactory _httpClient;
+        public StockService(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<string> GetStockPrice(string stockSymbol)
+        {
+            using (var client = _httpClient.CreateClient("StockApi"))
+            {
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"stock/{stockSymbol}/quote");
+                var response =  await client.SendAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    throw new Exception("Error fetching stock price");
+                }
+            }
+        }
+    }
+}
