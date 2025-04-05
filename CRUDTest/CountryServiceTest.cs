6ecb947a2a8d2167545a74530accc4d21731d2d4
Nishant Shingate <nishantks121@gmail.com>
2025-04-05 14:35:01 +0530

    Implemented and tested AddCountry.

--- CRUDTest/CountryServiceTest.cs
+++ CRUDTest/CountryServiceTest.cs
@@ -16,5 +16,62 @@ namespace CRUDTest
             countryService = new CountryService();
         }
 
+
+        // CountryRequestAdd is null then should throw ArgumentNullException
+        [Fact]
+        public void AddCountry_NullRequest()
+        {
+            // Arrange
+            CountryAddRequest countryRequestAdd = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => countryService.AddCountry(countryRequestAdd));
+        }
+        // CountryRequestAdd.CountryName is null then should throw ArgumentException
+        [Fact]
+        public void AddCountry_NullCountryName()
+        {
+            // Arrange
+            CountryAddRequest countryRequestAdd = new CountryAddRequest
+            {
+                CountryName = null
+            };
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => countryService.AddCountry(countryRequestAdd));
+        }
+        // CountryRequestAdd.CountryName is duplicate then should throw ArgumentException
+        [Fact]
+        public void AddCountry_DuplicateCountryName()
+        {
+            // Arrange
+            CountryAddRequest countryRequestAdd1 = new CountryAddRequest
+            {
+                CountryName = "USA"
+            };
+            CountryAddRequest countryRequestAdd2 = new CountryAddRequest
+            {
+                CountryName = "USA"
+            };
+            countryService.AddCountry(countryRequestAdd1);
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => countryService.AddCountry(countryRequestAdd2));
+        }
+        // CountryRequestAdd is valid then it should add the country and return the country response
+        [Fact]
+        public void AddCountry_ValidRequest()
+        {
+            // Arrange
+            CountryAddRequest countryRequestAdd = new CountryAddRequest
+            {
+                CountryName = "INDIA"
+            };
+            // Act
+            var response = countryService.AddCountry(countryRequestAdd);
+            // Assert
+            Assert.NotNull(response);
+            Assert.Equal("INDIA", response.CountryName);
+            Assert.NotNull(response.CountryId);
+        }
+
     }
 }
--- Services/CountryService.cs
+++ Services/CountryService.cs
@@ -1,11 +1,39 @@
 ﻿using ServiceContracts;
+using Entities;
+using System.Runtime.CompilerServices;
 namespace Services
 {
     public class CountryService : ICountryService
     {
+        private List<Country> _countries = new List<Country>();
         public CountryResponse AddCountry(CountryAddRequest countryAddRequest)
         {
-            throw new NotImplementedException();
+            // request if null
+            if(countryAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(countryAddRequest));
+            }
+
+            if(countryAddRequest.CountryName == null)
+            {
+                throw new ArgumentException(nameof(countryAddRequest.CountryName));
+            }
+
+            if(_countries.Where(tmp => tmp.CountryName == countryAddRequest.CountryName).Any())
+            {
+                throw new ArgumentException("Country already exists");
+            }
+
+            //request is valid
+            Country country = new Country
+            {
+                CountryName = countryAddRequest.CountryName,
+                CountryId = Guid.NewGuid(),
+            };
+
+            _countries.Add(country);
+            return country.ToCountryResponse();
+
         }
     }
 }
