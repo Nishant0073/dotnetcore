ce77a0e66c817ddc43048e2397a9e3d730f89a85
Nishant Shingate <nishantks121@gmail.com>
2025-04-06 15:56:41 +0530

    Tested & Implemented GetAllPerson

--- CRUDTest/PersonServiceTest.cs
+++ CRUDTest/PersonServiceTest.cs
@@ -4,6 +4,7 @@ using Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,7 +18,7 @@ namespace CRUDTest
         {
             _personService = new PersonService();
             _countryService = new CountryService();
-            
+
         }
         #region AddPerson
 
@@ -49,7 +50,8 @@ namespace CRUDTest
 
         //when person is valid should return PersonResponse
         [Fact]
-        public void AddPerson_ValidRequest() {
+        public void AddPerson_ValidRequest()
+        {
             // Arrange
             PersonAddRequest personRequestAdd = new PersonAddRequest
             {
@@ -66,7 +68,7 @@ namespace CRUDTest
             var response_from_getAllPerson = _personService.GetAllPersons();
 
             // Assert
-            Assert.True(response_from_add_person.PersonId!=Guid.Empty);
+            Assert.True(response_from_add_person.PersonId != Guid.Empty);
             Assert.Contains(response_from_add_person, response_from_getAllPerson);
 
         }
@@ -103,11 +105,86 @@ namespace CRUDTest
                 RecieveNewsLetter = true
             };
 
-           PersonResponse personResponse_from_AddPerson = _personService.AddPerson(personAddRequest);
+            PersonResponse personResponse_from_AddPerson = _personService.AddPerson(personAddRequest);
 
             PersonResponse personResponse_from_GetPersonById = _personService.GetPersonById(personResponse_from_AddPerson.PersonId);
 
             Assert.Equal(personResponse_from_AddPerson.PersonId, personResponse_from_GetPersonById.PersonId);
+        }
+        #endregion
+
+
+        #region GetAllPersons
+        [Fact]
+        public void GetAllPersons_EmptyList()
+        {
+            // Arrange & Act
+            var persons = _personService.GetAllPersons();
+            // Assert
+            Assert.Empty(persons);
+        }
+
+        [Fact]
+        public void GetAllPerson_validRequest()
+        {
+            CountryAddRequest countryAddRequest1 = new CountryAddRequest
+            {
+                CountryName = "USA"
+            };
+
+            CountryAddRequest countryAddRequest2 = new CountryAddRequest
+            {
+                CountryName = "Canada"
+            };
+
+            CountryResponse country_from_AddCountry1 = _countryService.AddCountry(countryAddRequest1);
+            CountryResponse country_from_AddCountry2 = _countryService.AddCountry(countryAddRequest2);
+
+            List<PersonAddRequest> personAddRequests = new List<PersonAddRequest>{
+                new PersonAddRequest
+                {
+                    PersonName = "Alice Smith",
+                    Email = "alice.smith@example.com",
+                    DateOfBirth = new DateTime(1990, 5, 15),
+                    Gender = ServiceContracts.Enum.GenderOptions.Female,
+                    CountryId = country_from_AddCountry1.CountryId,
+                    Address = "456 Elm St",
+                    RecieveNewsLetter = true
+                },
+                new PersonAddRequest
+                {
+                    PersonName = "Bob Johnson",
+                    Email = "bob.johnson@example.com",
+                    DateOfBirth = new DateTime(1985, 8, 20),
+                    Gender = ServiceContracts.Enum.GenderOptions.Male,
+                    CountryId = country_from_AddCountry2.CountryId,
+                    Address = "789 Maple St",
+                    RecieveNewsLetter = false
+                },
+                new PersonAddRequest
+                {
+                    PersonName = "Charlie Brown",
+                    Email = "charlie.brown@example.com",
+                    DateOfBirth = new DateTime(2000, 12, 25),
+                    Gender = ServiceContracts.Enum.GenderOptions.Other,
+                    CountryId = country_from_AddCountry1.CountryId,
+                    Address = "101 Pine St",
+                    RecieveNewsLetter = true
+                },
+            };
+
+            List<PersonResponse> personResponses_from_AddPerson = new List<PersonResponse>();
+            foreach(PersonAddRequest personAddRequest in personAddRequests)
+            {
+                PersonResponse personResponse_from_AddPerson = _personService.AddPerson(personAddRequest);
+                personResponses_from_AddPerson.Add(personResponse_from_AddPerson);
+            }
+            List<PersonResponse> personResponses_from_GetAllPersons = _personService.GetAllPersons();
+
+            foreach (var person in personResponses_from_AddPerson)
+            {
+                Assert.Contains(person, personResponses_from_GetAllPersons);
+            }
         }
         #endregion
     }
