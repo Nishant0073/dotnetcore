b8f79fc68908bbce82c47bd64d2221647488ec29
Nishant Shingate <nishantks121@gmail.com>
2025-04-19 16:45:12 +0530

    Added PersonDbContext

---
+++ EntityFramework/CRUDExample/Program.cs
@@ -0,0 +1,26 @@
+using ServiceContracts;
+using Services;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+//add services into IoC container
+builder.Services.AddSingleton<ICountriesService, CountriesService>();
+builder.Services.AddSingleton<IPersonsService, PersonsService>();
+builder.Services.AddDbContext<PersonDbContext>(options => options.UseSqlServer());
+
+
+var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+  app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
---
+++ EntityFramework/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    /// <summary>
+    /// Domain Model for Country
+    /// </summary>
+    public class Country
+    {
+        [Key]
+        public Guid CountryID { get; set; }
+
+        [StringLength(100)]
+        public string? CountryName { get; set; }
+    }
+}
