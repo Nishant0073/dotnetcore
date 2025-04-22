c7cd55de2fca7fa7a81da05f0716f8b678b5a971
Nishant Shingate <nishantks121@gmail.com>
2025-04-22 23:31:51 +0530

    feat: Added extra coloumn to person.

---
+++ EntityFramework/Entities/Migrations/20250422180034_TINColoumn.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    public partial class TINColoumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TIN",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TIN",
+                table: "Persons");
+        }
+    }
+}
--- EntityFramework/Entities/Person.cs
+++ EntityFramework/Entities/Person.cs
@@ -3,32 +3,35 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Entities
 {
-  /// <summary>
-  /// Person domain model class
-  /// </summary>
-  public class Person
-  {
-    [Key]
-    public Guid PersonID { get; set; }
+    /// <summary>
+    /// Person domain model class
+    /// </summary>
+    public class Person
+    {
+        [Key]
+        public Guid PersonID { get; set; }
 
-    [StringLength(40)] //nvarchar(40)
-    public string? PersonName { get; set; }
+        [StringLength(40)] //nvarchar(40)
+        public string? PersonName { get; set; }
 
-    [StringLength(40)] //nvarchar(40)
-    public string? Email { get; set; }
+        [StringLength(40)] //nvarchar(40)
+        public string? Email { get; set; }
 
-    public DateTime? DateOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
 
-    [StringLength(10)] //nvarchar(100)
-    public string? Gender { get; set; }
+        [StringLength(10)] //nvarchar(100)
+        public string? Gender { get; set; }
 
-    //uniqueidentifier
-    public Guid? CountryID { get; set; }
+        //uniqueidentifier
+        public Guid? CountryID { get; set; }
 
-    [StringLength(200)] //nvarchar(200)
-    public string? Address { get; set; }
+        [StringLength(200)] //nvarchar(200)
+        public string? Address { get; set; }
 
-    //bit
-    public bool ReceiveNewsLetters { get; set; }
-  }
+        //bit
+        public bool ReceiveNewsLetters { get; set; }
+
+        public string? TIN { get; set; } //Tax Identification Number
+
+    }
 }
